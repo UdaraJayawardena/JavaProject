294e12b4d3a825694a0f39cbb903c7365c81c701
UdaraJayawardena <udarajaywardena5@gmail.com>
2020-01-10 13:04:27 +0530

    'newedit'

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-class Hello {
+Class Hello {
 	public static void main(String args[]){
 		for(int i=0; i<10; i++){
-			System.out.println("for loop");	
+			System.out.println("for loop 2020");	
 		}
 	}
 }
